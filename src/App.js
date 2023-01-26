14dde6aea700df21ffe9f19e7916ec32c8da6edd
youss719 <76074058+youss719@users.noreply.github.com>
2023-01-26 08:08:19 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import FirstForm from './FirstForm';
+import Home from './Home';
+import Header from './Header';
+import ContextualizationComponent from './ContextctualizationComponent';
+import TextInput from './TextInput';
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+
+      
+
+      <ContextualizationComponent/>
+
+      
+    </div>
+  );
+}
+
+export default App;
